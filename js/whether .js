80b6f4d0173231b4a1f343b255ae94ec4ac95fda
shifas <sshifas333@gmail.com>
2024-09-14 13:18:32 +0530

    old file updated

--- js/whether .js
+++ js/whether .js
@@ -1,25 +1,37 @@
-const apiKey="382910a4b8e885c416fcb677a59fdca7";
-const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-const searchBox=document.querySelector(".search input")
-const searchBtn=document.querySelector(".search button")
-async function checkWhether(city){
-    // try{
-        const res=await fetch(apiUrl+ city +`&appid=${apiKey}`);
-        var data=await res.json();
-        console.log(data);
-        document.querySelector(".city").innerHTML=data.name;
-        document.querySelector(".temperature").innerHTML=Math.round(data.main.temp)+"°C";
-        document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
-        document.querySelector(".wind").innerHTML=Math.round(data.wind.speed)+"km/h";
+// const apiKey="382910a4b8e885c416fcb677a59fdca7";
+// const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+// const searchBox=document.querySelector(".search input")
+// const searchBtn=document.querySelector(".search button")
+// async function checkWhether(city){
+//     // try{
+//         const res=await fetch(apiUrl+ city +`&appid=${apiKey}`);
+//         var data=await res.json();
+//         console.log(data);
+//         document.querySelector(".city").innerHTML=data.name;
+//         document.querySelector(".temperature").innerHTML=Math.round(data.main.temp)+"°C";
+//         document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
+//         document.querySelector(".wind").innerHTML=Math.round(data.wind.speed)+"km/h";
     
-// }catch(error){
-//  console.log(error);
+// // }catch(error){
+// //  console.log(error);
  
+// // }
+
 // }
 
+// searchBtn.addEventListener("click",()=>{
+//     checkWhether(searchBox.value);
+
+// })
+
+
+async function asd(){
+    // alert("asas")
+    let inp=document.getElementById("sear").value;
+     const data=await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${inp}&appid=382910a4b8e885c416fcb677a59fdca7`);
+     var res=await data.json();
+     console.log(res);
+     
+
+    
 }
-
-searchBtn.addEventListener("click",()=>{
-    checkWhether(searchBox.value);
-
-})
